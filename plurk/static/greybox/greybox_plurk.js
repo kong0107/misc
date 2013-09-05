f59a68e2b1d7b7847c8421596c44c23a5d86b8e8
kong0107 <kong0107@gmail.com>
2013-09-06 00:23:40 +0800

    plurk backup 20130903

--- plurk/static/greybox/greybox_plurk.js
+++ plurk/static/greybox/greybox_plurk.js
@@ -1,2 +1,14 @@
 var loc = window.location;
 var GB_ROOT_DIR = loc.protocol + '//' +  loc.host + "/static/greybox/";
+
+function showGBRegister() {
+    if (window.GB_showCenter)
+		GB_showCenter(_('Sign up for Plurk'), '/Users/showRegister?overlay=1', 400, 700);
+	return false;
+};
+
+function showGBLogin() {
+    if (window.GB_showCenter)
+		GB_showCenter(_('Plurk sign in'), '/Users/showLogin?overlay=1', 400, 700);
+	return false;
+};
