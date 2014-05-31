56ebc65692fb04036cc35637f36f90e1d6a31147
Kong Kao <kong0107@gmail.com>
2014-05-31 15:40:39 +0800

    plurk backup 20140209

--- plurk/static/greybox/greybox_plurk.js
+++ plurk/static/greybox/greybox_plurk.js
@@ -2,15 +2,39 @@ var loc = window.location;
 var GB_ROOT_DIR = loc.protocol + '//' +  loc.host + "/static/greybox/";
 
 function showGBRegister() {
-    if (window.GB_showCenter)
-		GB_showCenter(_('Sign up for Plurk'), '/Users/showRegister?overlay=1', 400, 700);
-	return false;
+
+    if (!jQuery || jQuery(window).width() < 525 || jQuery(window).height() < 600){
+        return true;
+    }
+
+    if (jQuery(window).width() < 768){
+		GB_showFullScreen(_('Sign up for Plurk'), '/Users/showRegister?overlay=1');
+		return false;
+    }
+
+    if (window.GB_showCenter){
+		GB_showCenter(_('Sign up for Plurk'), '/Users/showRegister?overlay=1', 400, 660);
+        	return false;
+    }
+    
+    return true;
 };
 
 function showGBLogin() {
-    if (window.GB_showCenter)
-		GB_showCenter(_('Plurk sign in'), '/Users/showLogin?overlay=1', 400, 700);
-	return false;
+    if (!jQuery || jQuery(window).width() < 320 || jQuery(window).height() < 400) 
+        return true;
+    
+    if (jQuery(window).width() < 768 && window.GB_showFullScreen){
+		GB_showFullScreen(_('Plurk sign in'), '/Users/showLogin?overlay=1');
+        return false;
+    }
+
+    if (window.GB_showCenter){
+		GB_showCenter(_('Plurk sign in'), '/Users/showLogin?overlay=1', 400, 660);
+        	return false;
+    }
+    
+    return true;    
 };
 
 
